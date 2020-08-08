0e0f658fb808d66659a90ad288be8f8ddc5a1803
github username <github email address>
2020-08-07 20:16:52 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,10 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    set_song
   end
 
   def new
@@ -21,29 +23,28 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    set_song
   end
 
   def update
-    @song = Song.find(params[:id])
+    set_song
 
     @song.update(song_params)
 
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
-    end
   end
 
   def destroy
-    @song = Song.find(params[:id])
+    set_song
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
   end
 
   private
+
+  def set_song 
+    @song = Song.find_by(params[:id])
+  end
 
   def song_params
     params.require(:song).permit(:title)
